dd4801f15adefca3f16b20b5e2d14d29dcf4534a
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-02-25 11:51:44 -0600

    Create module index.

--- src/init.lua
+++ src/init.lua
@@ -6,4 +6,18 @@
       \ \ \L\ \/\ \__/\ \ \ \ \/\ \L\ \
        \ \____/\ \____\\ \_\ \_\ \____/
         \/___/  \/____/ \/_/\/_/\/___/ 
+
+    Github: https://github.com/monke-mob/echo
 ]]
+
+local RunService = game:GetService("RunService")
+
+local environment: ModuleScript
+
+if RunService:IsClient() then
+    environment = script.client
+else
+    environment = script.server
+end
+
+return require(environment)
