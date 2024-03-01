403e40e2bc30c0c3b82617b40ebb5c0c5146ffe0
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-01 13:08:08 -0600

    Improve server environment.

--- src/environments/server.lua
+++ src/environments/server.lua
@@ -1,50 +1,41 @@
 local baseEnvironment = require(script.Parent.base)
-local createAudio = require(script.Parent.Parent.functions.createAudio)
 local generateAudioID = require(script.Parent.Parent.functions.generateAudioID)
-local getPlayParameters = require(script.Parent.Parent.functions.getPlayParameters)
+local types = require(script.Parent.Parent.types)
 local playEvent: RemoteEvent = script.Parent.Parent.events.play
-local stopEvent: RemoteEvent = script.Parent.Parent.events.stop
-
-type audio = baseEnvironment.audio & {
-    instance: Sound?,
-    replicate: boolean,
-    replicateData: {
-        startTime: number,
-        audioID: string,
-        parent: Instance?,
-        position: number?,
-    }?,
-}
 
 --[[
     Controls the server.
 
     @public
 ]]
-local server = baseEnvironment
+local controller = baseEnvironment
+
+export type controller = baseEnvironment.controller & {
+    start: () -> never,
+    play: (properties: types.properties, id: string?, group: string?) -> string,
+    playOnServer: (properties: types.properties, id: string?, group: string?) -> Sound,
+}
 
 --[[
     Starts the server environment.
 
     @returns never
 ]]
-function server:start()
-    self:baseStart()
+function controller:start()
+    self:_start()
 
     playEvent.OnServerEvent:Connect(function(player: Player)
-        for id: string, audio: audio in pairs(self._audios) do
-            if audio.replicate ~= true or audio.replicateData == nil then
+        for id: string, audio: types.audio in pairs(self._audios) do
+            if audio.replicates ~= true or audio.metadata == nil then
                 continue
             end
 
-            playEvent:FireClient(
-                player,
-                audio.replicateData.audioID,
-                audio.replicateData.parent,
-                audio.group,
-                id,
-                audio.replicateData.startTime - os.clock() + (audio.replicateData.position or 0)
-            )
+            -- Update the position so that the client is at the same position as all the others.
+            local position: number = self:_getProperty(audio.metadata, "Position", 0)
+            local startTime: number = self:_getProperty(audio.metadata, "startTime", os.clock())
+            audio.metadata["Position"] = startTime - os.clock() + position
+
+            playEvent:FireClient(player, audio.metadata, id, audio.group)
         end
     end)
 end
@@ -52,95 +43,38 @@ end
 --[[
     Plays a audio on the clients.
 
-    @param {string} audioID [The ID of the audio.]
-    @param {Instance?} parent [The parent to be used for the audio instance.]
-    @param {string?} group [The ID of the audio group.]
-    @param {boolean?} persistent [Stores the audio in a group. WARNING: The audio will not be cleaned up automatically.]
+    @param {types.properties} properties [The audio properties.]
     @param {string?} id [The ID for accessing the audio through Echo.]
-    @param {number?} position [The starting position of the audio.]
+    @param {string?} group [The ID of the audio group.]
     @returns string
 ]]
-function server:play(
-    audioID: string,
-    parent: Instance?,
-    group: string?,
-    persistent: boolean?,
-    id: string?,
-    position: number?
-): string
+function controller:play(properties: types.properties, id: string?, group: string?): string
     if typeof(id) ~= "string" then
-        id = generateAudioID(audioID)
+        id = generateAudioID(properties.audioID)
     end
 
-    if persistent == true then
+    local persistent: boolean = self:_getProperty(properties, "persistent", false)
+
+    if persistent then
+        properties["startTime"] = os.clock()
         self._audios[id] = {
             instance = nil,
             group = group,
-            replicate = true,
-            replicateData = {
-                startTime = os.clock(),
-                audioID = audioID,
-                parent = parent,
-                position = position,
-            },
+            replicates = true,
+            metadata = properties,
         }
+    else
+        id = `replicatedNotPersistent-{id}`
     end
 
-    playEvent:FireAllClients(audioID, parent, group, id, position)
+    playEvent:FireAllClients(properties, id, group)
+
     return id :: string
 end
 
 --[[
-    Plays a audio on the server.
-
-    @param {string} audioID [The ID of the audio.]
-    @param {Instance?} parent [The parent to be used for the audio instance.]
-    @param {string?} group [The ID of the audio group.]
-    @param {string?} id [The ID for accessing the audio through Echo.]
-    @param {number?} position [The starting position of the audio.]
-    @returns Sound
+    @extends baseEnvironment._play
 ]]
-function server:playOnServer(audioID: string, parent: Instance?, group: string?, id: string?, position: number?): Sound
-    parent, group, id, position = getPlayParameters(audioID, parent, group, id, position)
+controller.playOnServer = controller._play
 
-    local audioInstance: Sound =
-        createAudio(audioID, parent :: Instance, group :: string, id :: string, position :: number)
-    audioInstance.Volume = self:getVolume(group)
-    audioInstance:Play()
-
-    audioInstance.Ended:Once(function()
-        self:stop(id)
-    end)
-
-    self._audios[id] = {
-        instance = audioInstance,
-        group = group,
-    }
-
-    return audioInstance
-end
-
---[[
-    Stops and removes a audio.
-
-    @param {string} id [The ID for accessing the audio through Echo.]
-    @returns never
-]]
-function server:stop(id: string)
-    local audio: audio? = self._audios[id]
-
-    if audio == nil then
-        stopEvent:FireAllClients(id)
-        return
-    end
-
-    if audio.replicate then
-        stopEvent:FireAllClients(id)
-        self._audios[id] = nil
-    end
-
-    audio.instance:Destroy()
-    self._audios[id] = nil
-end
-
-return server
+return (controller :: any) :: controller
