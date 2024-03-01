abd39ae4929314881c9ec2d47afb5d59636d17c8
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-01 07:31:13 -0600

    Rework base environment.

--- src/environments/base.lua
+++ src/environments/base.lua
@@ -1,24 +1,49 @@
-export type audio = {
-    instance: Sound,
-    group: string,
-}
+local RunService = game:GetService("RunService")
+local SoundService = game:GetService("SoundService")
+
+local propertyBlacklist: { string } = { "audioID", "handleCleanup", "Parent", "Volume", "Name" }
+
+local generateAudioID = require(script.Parent.Parent.functions.generateAudioID)
+local types = require(script.Parent.Parent.types)
+local stopEvent: RemoteEvent = script.Parent.Parent.events.stop
 
 --[[
     Controls the base environment.
 
     @public
 ]]
-local base = {}
-base._volume = {}
-base._audios = {}
+local controller = {}
+controller._groups = {}
+controller._audios = {}
+
+export type controller = {
+    _groups: { [string]: number },
+    _audios: { [string]: types.audio },
+    setVolume: (volume: number, group: string?) -> never,
+    getVolume: (group: string?) -> number,
+    _start: () -> never,
+    _play: (properties: types.properties, id: string?, group: string?) -> Sound,
+    _getProperty: (properties: types.properties, property: string, default: types.property) -> types.property,
+}
 
 --[[
-    Starts the environment.
+    Stops and destroys an audio.
 
+    @param {string?} id [The ID for accessing the audio through Echo.]
     @returns never
 ]]
-function base:baseStart()
-    self:setVolume(1)
+function controller:stop(id: string)
+    local audio: types.audio? = self._audios[id]
+
+    if audio == nil then
+        return
+    elseif audio.instance then
+        audio.instance:Destroy()
+    elseif RunService:IsServer() and audio.replicates then
+        stopEvent:FireAllClients(id)
+    end
+
+    self._audios[id] = nil
 end
 
 --[[
@@ -28,14 +53,14 @@ end
     @param {string?} group [The ID of the audio group.]
     @returns never
 ]]
-function base:setVolume(volume: number, group: string?)
+function controller:setVolume(volume: number, group: string?)
     if typeof(group) ~= "string" then
         group = "default"
     end
 
-    self._volume[group] = volume
+    self._groups[group] = volume
 
-    for _id: string, audio: audio in pairs(self._audios) do
+    for _id: string, audio: types.audio in pairs(self._audios) do
         if audio.group ~= group then
             continue
         end
@@ -50,12 +75,100 @@ end
     @param {string?} group [The ID of the audio group.]
     @returns number
 ]]
-function base:getVolume(group: string?): number
+function controller:getVolume(group: string?): number
     if typeof(group) ~= "string" then
         group = "default"
     end
 
-    return self._volume[group] or 0
+    return self._groups[group] or 0
 end
 
-return base
+--[[
+    Starts the environment.
+
+    @returns never
+]]
+function controller:_start()
+    self:setVolume(1)
+end
+
+--[[
+    Plays a audio.
+
+    @private
+    @param {types.properties} properties [The audio properties.]
+    @param {string?} id [The ID for accessing the audio through Echo.]
+    @param {string?} group [The ID of the audio group.]
+    @returns Sound
+]]
+function controller:_play(properties: types.properties, id: string?, group: string?): Sound
+    if typeof(id) ~= "string" then
+        id = generateAudioID(properties.audioID)
+    end
+
+    local audioInstance: Sound = Instance.new("Sound")
+    audioInstance.Name = self:_getProperty(properties, "Name", id)
+    audioInstance.SoundId = properties.audioID
+    audioInstance.Parent =
+        self:_getProperty(properties, "Parent", if RunService:IsClient() then SoundService else workspace)
+    audioInstance.Volume = self:_getProperty(properties, "Volume", self:getVolume(group))
+
+    for property: string, value: types.property in pairs(properties) do
+        -- Some properties should not be set via this loop, so we skip those.
+        if table.find(propertyBlacklist, property) or audioInstance[property] == nil then
+            continue
+        end
+
+        audioInstance[property] = value
+    end
+
+    local destroyOnEnded: boolean = self:_getProperty(properties, "destroyOnEnded", true)
+
+    -- If the audio is on loop then the Ended signal will never be called.
+    if destroyOnEnded and audioInstance.Looped ~= true then
+        audioInstance.Ended:Once(function()
+            self:stop(id)
+        end)
+    else
+        -- The audio will not be destroyed by Echo automatically so in case the developer does not use
+        -- the stop method then a connection to the Destroying signal is needed so that the audio can be
+        -- removed internally.
+        audioInstance.Destroying:Connect(function()
+            self:stop(id)
+        end)
+
+        if destroyOnEnded and audioInstance.Looped then
+            -- Warn the developer of the error to make it more clear on the issue.
+            warn(
+                "[Echo] destroyOnEnded property was set true but the audio was also looped. If you wish for Echo to handle cleanup of the audio then set looped to false."
+            )
+        end
+    end
+
+    self._audios[id] = {
+        instance = audioInstance,
+        group = group,
+        replicates = false,
+    }
+
+    return audioInstance
+end
+
+--[[
+    Returns a user passed property value or returns its default value.
+
+    @private
+    @param {types.properties} properties [The audio properties.]
+    @param {string} property [The property name.]
+    @param {types.property} default [The default property value.]
+    @returns types.property
+]]
+function controller:_getProperty(
+    properties: types.properties,
+    property: string,
+    default: types.property
+): types.property
+    return if properties[property] then properties[property] else default
+end
+
+return (controller :: any) :: controller
