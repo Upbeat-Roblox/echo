7a7f574295af0ee0b836b07232e4b690981de582
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-01 07:40:08 -0600

    Make metadata optional.

--- src/types.lua
+++ src/types.lua
@@ -2,7 +2,7 @@ export type audio = {
     instance: Sound,
     group: string,
     replicates: boolean,
-    metadata: { [string]: property },
+    metadata: { [string]: property }?,
 }
 
 export type property = string | number | boolean
