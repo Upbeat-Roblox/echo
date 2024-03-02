dbe05d2993033cf4ad39fcb89a5ff1e391a12bc9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-02 09:38:15 -0600

    Rename _start to _baseStart.

--- src/environments/base/queue.lua
+++ src/environments/base/queue.lua
@@ -33,7 +33,7 @@ export type controller = {
     restart: () -> never,
     getCurrentAudioMetadata: () -> types.metadata,
     skipTo: (id: number | string) -> never,
-    _start: () -> never,
+    _baseStart: () -> never,
     _playIndex: (index: number) -> never,
     _findIndexByID: (id: string) -> number?,
 }
@@ -252,7 +252,7 @@ end
     @private
     @returns never
 ]]
-function controller:_start()
+function controller:_baseStart()
     self:setQueue("default")
     environmentController:setVolume("queue", 1)
 end
