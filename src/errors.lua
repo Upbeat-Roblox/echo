7bca3882f65b51f821535ef3a9aba6791e9d225d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-02 09:32:43 -0600

    Add audioIDDoesNotExist error.

--- src/errors.lua
+++ src/errors.lua
@@ -2,4 +2,5 @@ return {
     blacklistedQueueID = "%s is a blacklisted queue ID for the method %s.",
     queueCreateIDError = "Queue, %s, already exists.",
     queueDoesNotExist = "Queue, %s, does not exist.",
+    audioIDDoesNotExist = "The audio ID, %s, does not exist.",
 }
